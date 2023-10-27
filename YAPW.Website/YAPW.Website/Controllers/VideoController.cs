0744f9cd9cf059dd0bed71cd7e24dc7ab3b8e391
Nikunj Rathod <nikunjrathod3011@gmail.com>
2023-10-27 14:54:12 -0300

    update caching in website and call updateview method for video views

--- YAPW.Website/YAPW.Website/Controllers/VideoController.cs
+++ YAPW.Website/YAPW.Website/Controllers/VideoController.cs
@@ -159,6 +159,28 @@ public class VideoController : BaseController
         }
     }
 
+    public async Task<(VideoDataModel video, List<VideoDataModel> videosRandom, List<VideoDataModel> videosSameBrand, List<VideoDataModel> videosSameSeries)> GetVideoInfo(string id)
+    {
+        var video = await ExecuteServiceRequest<VideoDataModel>(HttpMethod.Get, $"Videos/detailed/" + id);
+        video.VideoInfo.VideoUrl = $"https://r2.1hanime.com/{video.VideoInfo.VideoUrl}.mp4";
+        //if next video is present add it
+        //featured, recently addded, random, same brand
+        //ViewBag.VideosFeatured = await ExecuteServiceRequest<List<VideoDataModel>>(HttpMethod.Get, $"Videos/featured/byViews/19");
+        //ViewBag.VideosRecentlyAdded = await ExecuteServiceRequest<List<VideoDataModel>>(HttpMethod.Get, $"Videos/newReleases/19");
+        var videosRandom = await ExecuteServiceRequest<List<VideoDataModel>>(HttpMethod.Get, $"Videos/random/19");
+        var videosSameBrand = await ExecuteServiceRequest<List<VideoDataModel>>(HttpMethod.Get, $"Videos/brand/19/" + video.Brand.Name.ToLower());
+
+        //if other parts exists show next or previous button
+        string pattern = @"\d+$";
+        string replacement = "";
+        Regex rgx = new Regex(pattern);
+        string trimmedName = rgx.Replace(id.Trim(), replacement);
+        trimmedName = trimmedName.Replace("-", " ");
+
+        var videosSameSeries = await ExecuteServiceRequest<List<VideoDataModel>>(HttpMethod.Get, $"Videos/SearchByName/" + trimmedName);
+        return (video, videosRandom,  videosSameBrand, videosSameSeries);
+    }
+
     //[AjaxOnly]
     //[HttpGet("Name/{name}")]
     [OutputCache(Duration = 3600, PolicyName = "VideosPolicy")]
@@ -170,79 +192,16 @@ public class VideoController : BaseController
             {
                 cache = await CacheData();
             }
-            var videos = cache.latestVideos;
             ViewBag.VideosTrending = cache.trendingVideos;
-
-            var video = await ExecuteServiceRequest<VideoDataModel>(HttpMethod.Get, $"Videos/detailed/" + id);
-            video.VideoInfo.VideoUrl = $"https://r2.1hanime.com/{video.VideoInfo.VideoUrl}.mp4";
-            //if next video is present add it
-            //featured, recently addded, random, same brand
-            //ViewBag.VideosFeatured = await ExecuteServiceRequest<List<VideoDataModel>>(HttpMethod.Get, $"Videos/featured/byViews/19");
             ViewBag.VideosFeatured = cache.trendingVideos.Take(19).ToList();
-            //ViewBag.VideosRecentlyAdded = await ExecuteServiceRequest<List<VideoDataModel>>(HttpMethod.Get, $"Videos/newReleases/19");
             ViewBag.VideosRecentlyAdded = cache.latestVideos.Take(19).ToList();
-            ViewBag.VideosRandom = await ExecuteServiceRequest<List<VideoDataModel>>(HttpMethod.Get, $"Videos/random/19");
-            ViewBag.VideosSameBrand = await ExecuteServiceRequest<List<VideoDataModel>>(HttpMethod.Get, $"Videos/brand/19/" + video.Brand.Name.ToLower());
+            ViewBag.Id = id;
+            var video = await GetVideoInfo(id);
 
-            //if other parts exists show next or previous button
-            string pattern = @"\d+$";
-            string replacement = "";
-            Regex rgx = new Regex(pattern);
-            string trimmedName = rgx.Replace(id.Trim(), replacement);
-            trimmedName = trimmedName.Replace("-", " ");
-
-            ViewBag.VideosSameSeries = await ExecuteServiceRequest<List<VideoDataModel>>(HttpMethod.Get, $"Videos/SearchByName/" + trimmedName);
-
-
-            //update likes, dislikes, views in api
-            //update when there is low user count
-            //use metrics servers to get data
-            //or get api metrics if low post to endpoint
-            var cacheKeyVideoLikesList = "videoLikesList";
-
-            if (!_memoryCache.TryGetValue(cacheKeyVideoLikesList, out Dictionary<string, int> cacheVideoLikesList))
-            {
-                cacheVideoLikesList = new Dictionary<string, int>();
-
-                //setting up cache options
-                var cacheExpiryOptions = new MemoryCacheEntryOptions
-                {
-                    AbsoluteExpiration = DateTime.Now.AddMinutes(30),
-                    Priority = CacheItemPriority.High,
-                    SlidingExpiration = TimeSpan.FromMinutes(30)
-                }.RegisterPostEvictionCallback(async (key, value, reason, substate) =>
-                {
-                    //before cache is removed update those values
-                    try
-                    {
-                        //todo later add these calls directly through servicebroker and also do this for likes and dislikes
-                        var updateViews = await ExecutePutServiceRequest<string, Dictionary<string, int>>($"Videos/updateViews/", cacheVideoLikesList);
-                        cacheVideoLikesList = new Dictionary<string, int>();
-                    }
-                    catch (Exception ex)
-                    {
-                        throw new Exception("lol " + ex.Message);
-                    }
-                });
-                //setting cache entries
-
-                _memoryCache.Set(cacheKeyVideoLikesList, cacheVideoLikesList, cacheExpiryOptions);
-            }
-
-            if (!cacheVideoLikesList.ContainsKey(id))
-            {
-                cacheVideoLikesList.Add(id, 1);
-                videoLikesList.Add(id, 1);
-            }
-            else
-            {
-                var count = cacheVideoLikesList.GetValueOrDefault(id);
-                count++;
-                cacheVideoLikesList[id] = count;
-            }
-
-
-            return PartialView("Video", video);
+            ViewBag.VideosRandom = video.videosRandom;
+            ViewBag.VideosSameBrand = video.videosSameBrand;
+            ViewBag.VideosSameSeries = video.videosSameSeries;
+            return PartialView("Video", video.video);
         }
         catch (Exception ex)
         {
@@ -255,6 +214,56 @@ public class VideoController : BaseController
         }
     }
 
+    public async Task<IActionResult> UpdateVideoViews(string id)
+    {
+        //update likes, dislikes, views in api
+        //update when there is low user count
+        //use metrics servers to get data
+        //or get api metrics if low post to endpoint
+        var cacheKeyVideoLikesList = "videoLikesList";
+
+        if (!_memoryCache.TryGetValue(cacheKeyVideoLikesList, out Dictionary<string, int> cacheVideoLikesList))
+        {
+            cacheVideoLikesList = new Dictionary<string, int>();
+
+            //setting up cache options
+            var cacheExpiryOptions = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpiration = DateTime.Now.AddMinutes(30),
+                Priority = CacheItemPriority.High,
+                SlidingExpiration = TimeSpan.FromMinutes(30)
+            }.RegisterPostEvictionCallback(async (key, value, reason, substate) =>
+            {
+                //before cache is removed update those values
+                try
+                {
+                    //todo later add these calls directly through servicebroker and also do this for likes and dislikes
+                    var updateViews = await ExecutePutServiceRequest<string, Dictionary<string, int>>($"Videos/updateViews/", cacheVideoLikesList);
+                    cacheVideoLikesList = new Dictionary<string, int>();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("lol " + ex.Message);
+                }
+            });
+            //setting cache entries
+
+            _memoryCache.Set(cacheKeyVideoLikesList, cacheVideoLikesList, cacheExpiryOptions);
+        }
+
+        if (!cacheVideoLikesList.ContainsKey(id))
+        {
+            cacheVideoLikesList.Add(id, 1);
+            videoLikesList.Add(id, 1);
+        }
+        else
+        {
+            var count = cacheVideoLikesList.GetValueOrDefault(id);
+            count++;
+            cacheVideoLikesList[id] = count;
+        }
+        return Ok();
+    }
     //[AjaxOnly]
     //[HttpGet]
     //[Route("Search")]
